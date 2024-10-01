c5eb659284f1f8ca6551abaeb347800df8ab90f4
Jorge Dominguez <jorge.dominguez@bvgaboo.com>
2024-10-01 13:20:25 -0600

    Refactor getWeatherForAllTickets function for parallel weather data fetching

--- backend/src/controller/weather.controller.ts
+++ backend/src/controller/weather.controller.ts
@@ -2,3 +2,15 @@ import { getCachedWeatherData } from "../services/weather-service.js";
 import { Ticket } from "../types.js";
 
 // Get weather data for a list of tickets
+// This function needs to improve performance by fetching weather data for all tickets in parallel using Promise.all
+
+export const getWeatherForAllTickets = async (tickets: Ticket[]) => {
+    // Map each ticket to a promise that fetches weather data
+    const weatherPromises = tickets.map(async (ticket) => {
+        return getCachedWeatherData(ticket.originAirport.latitude, ticket.originAirport.longitude);
+    });
+
+    // Wait for all promises to resolve
+    const weatherResults = await Promise.all(weatherPromises);
+    return weatherResults;
+}

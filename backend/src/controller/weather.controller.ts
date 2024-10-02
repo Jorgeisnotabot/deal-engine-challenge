d09f2b58b3a9dec8a5ce416c7c65221f151a5aed
Jorge Dominguez <jorge.dominguez@bvgaboo.com>
2024-10-02 09:32:06 -0600

    Remove weather-service.controller

--- backend/src/controller/weather.controller.ts
+++ backend/src/controller/weather.controller.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
 import { getWeatherForAllTickets } from "../services/weather-service.js";
 import { Ticket } from "../types.js";
+import { parseTicketsData } from "../utils/csv-parser.js";  // Import the parser
+import path from "path";
 
 export const getWeatherReportsController = async (req: Request, res: Response): Promise<void> => {
     try {
-        const tickets: Ticket[] = req.body.tickets;
+        // const tickets: Ticket[] = req.body.tickets;
+
+        // Assuming the CSV file is uploaded and accessible on the server
+        const filePath = path.resolve("./backend/data/dataset.csv");
+
+        // Parse the CSV to get the array of tickets
+        const tickets: Ticket[] = await parseTicketsData(filePath);
+
 
         if (!tickets || tickets.length === 0) {
             res.status(400).json({ message: "Tickets are required" });
